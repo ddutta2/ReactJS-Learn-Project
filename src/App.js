f2760ef972d6b46cf459f5c894b17f12d73b9090
ddutta2 <debojyotiduttac12@gmail.com>
2020-09-15 15:14:58 +0530

    props added in class component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Greet name="Debu" heroName="DD"/>
-      <Welcome/>
+      <Welcome name="Debu" heroName="DD"/>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 // Class based component
 class Welcome extends Component{
     render(){
-        return <h1>Class Component</h1>;
+    return <h1>Welcome {this.props.name} a.k.a {this.props.heroName}</h1>;
     }
 }
 
