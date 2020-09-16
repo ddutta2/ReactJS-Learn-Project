9882a9aed5adcb152cce3f331ba9adc558bd80db
ddutta2 <debojyotiduttac12@gmail.com>
2020-09-16 15:39:03 +0530

    functional event handling added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Welcome name="Debu" heroName="DD"/>
       <Message/>
       <Counter addValue="1"/>
+      <FunctionClick/>
     </div>
   );
 }
